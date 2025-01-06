4f4ff09dc758367a45c9318de2c8fcd46c206d2e
MasterX_823 <109917976+github-VS-user@users.noreply.github.com>
2025-01-06 19:59:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,24 @@ document.getElementById('orderForm').addEventListener('submit', function (event)
   // Appeler la fonction pour chercher les détails de la commande
   fetchOrderDetails(orderNumber);
 });
+
+// Gestion de la connexion Admin
+document.getElementById('adminLoginForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Empêche le rechargement de la page
+
+  // Récupérer le mot de passe saisi par l'utilisateur
+  const password = document.getElementById('adminPassword').value.trim();
+
+  // Mot de passe Admin à vérifier
+  const adminPassword = 'master3DAdmin';
+
+  // Vérification du mot de passe
+  if (password === adminPassword) {
+    // Si le mot de passe est correct, afficher le panneau Admin
+    document.getElementById('adminPanel').classList.remove('hidden');
+    document.getElementById('adminLoginForm').classList.add('hidden'); // Cacher le formulaire de connexion
+  } else {
+    // Si le mot de passe est incorrect, afficher une alerte
+    alert('Mot de passe incorrect.');
+  }
+});
